e1b280e61e0eaabf3d28f72945612c6c598de41b
Semanur <semanurrrarslan@gmail.com>
2022-10-17 19:39:34 +0300

    day one finish

--- introduction.js
+++ introduction.js
@@ -1,14 +1,18 @@
 // console.log('Bu yazı İntroduction js dosyanında yazıldı')
 
 //VERİ TİPLERİ
-console.log(typeof 'Semanur') //String
-console.log(typeof 6)  //number
-console.log(typeof true)  //Boolean
-console.log(typeof null)   //object
-console.log(typeof undefined) //undefined
+/*
+   console.log(typeof 'Semanur') //String
+   console.log(typeof 6)  //number
+   console.log(typeof true)  //Boolean
+   console.log(typeof null)   //object
+   console.log(typeof undefined) //undefined
+*/
+
 
 
 //DEĞİŞKEN TANIMLAMA
+
 /*
    let myLesson = 'javascript'
    let lessonNumber = 1
@@ -25,7 +29,6 @@ console.log(typeof undefined) //undefined
 
      bu şekilde virgül kullanarak da birden çok değişken tanımlayabiliriz
 */
-
 
 /*
    const PI =3.14
--- variable.js
+++ variable.js
@@ -14,7 +14,56 @@
   ve kullanımı ve bilgilendirmeyi klaylaştırır 
 */
 
+console.log('Hello World')
+
 let myExcercise = 'exercise'
-let excersizeNumber = 1.1
+let excersizeNumber = 1
 let isExcersizeOver = false
 let secondexercise = null
+let excerciseFinish = undefined
+console.log(myExcercise, excersizeNumber, isExcersizeOver, secondexercise, excerciseFinish)
+
+/*
+ let ile değişkene değer vermeden değişken tanımlayabiliriz ama const ile bunu yapamayız  
+ */
+
+let serverName;
+console.log(serverName)
+
+let nameF;
+console.log(nameF)
+
+let secondName;
+console.log(secondName)
+
+let ageNumber;
+console.log(ageNumber)
+
+//değer vererek tanımlama
+
+let todayName = 'Monday'
+let todayNumber = 17
+let isTheFirstDayOfTheWeek = true
+let hour = undefined
+
+let firstName = 'Semanur'
+let lastName = 'Arslan'
+let maritalStatus = 'single'
+let country = 'Türkiye'
+let age = 23
+
+console.log(firstName, lastName, maritalStatus, country, age)
+
+
+/* örnek ikinci kullanım
+  let firstName = 'Semanur',
+     lastName = 'Arslan',
+     marialStatus = 'single',
+     country = 'Türkiye'
+     age = 23
+*/
+
+const myAge = 'I am 25 years old.'
+const yourAge = 'You are 30 years old.'
+
+console.log(myAge, yourAge)
