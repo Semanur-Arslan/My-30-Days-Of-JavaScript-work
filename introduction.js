46d00400dad4cc3993d995bff79013dfef73aa8b
Semanur <semanurrrarslan@gmail.com>
2022-10-17 19:02:52 +0300

    exercise yaıplıyor

---
+++ introduction.js
@@ -0,0 +1,35 @@
+// console.log('Bu yazı İntroduction js dosyanında yazıldı')
+
+//VERİ TİPLERİ
+console.log(typeof 'Semanur') //String
+console.log(typeof 6)  //number
+console.log(typeof true)  //Boolean
+console.log(typeof null)   //object
+console.log(typeof undefined) //undefined
+
+
+//DEĞİŞKEN TANIMLAMA
+/*
+   let myLesson = 'javascript'
+   let lessonNumber = 1
+   let isLessonContinue = true
+   let secondLesson = null
+   let lessonFinish = undefined
+*/
+
+/* let myLesson = 'javascript' ,
+     lessonNumber = 1 ,
+     isLessonContinue = true ,
+     secondLesson = null ,
+     lessonFinish = undefined
+
+     bu şekilde virgül kullanarak da birden çok değişken tanımlayabiliriz
+*/
+
+
+/*
+   const PI =3.14
+   const GRAVITY = 9.81
+   console.log(myLesson, lessonNumber, isLessonContinue, secondLesson, lessonFinish)
+   console.log(PI, GRAVITY)
+*/
---
+++ variable.js
@@ -0,0 +1,20 @@
+// EXERCİSE START
+
+// comments can make code readable
+// yorumlar codu okunabilir kılar
+
+// Welcome to 30DaysOfJavaScript
+
+/* 
+  comments can make code readable,
+  easy to reuse and informative 
+*/
+/*
+  yorumlar kodu okunabilir kılar 
+  ve kullanımı ve bilgilendirmeyi klaylaştırır 
+*/
+
+let myExcercise = 'exercise'
+let excersizeNumber = 1.1
+let isExcersizeOver = false
+let secondexercise = null
