140fa6f6e7330512c7f04692bead4a9975823b76
Semanur <semanurrrarslan@gmail.com>
2022-11-14 10:54:18 +0300

    bölüm sonu çalışması

--- DOM/dom2.js
+++ DOM/dom2.js
@@ -17,8 +17,33 @@ başlıkEkle.addEventListener('click',eklenenYazı)
 // parantez içine yazılacak olan diğer şey ise fonksiyondur. belirttiğimiz event gerçekleştirildiğinde hangi fonksiyonun çalışacağını buraya yazmamız gerekir.
 
 
+// BÖLÜM SONU ÇALIŞMASI
+let sonuç = 0 ;
+let sonuçDOM = document.querySelector('#sonuç');
+let arttırDOM = document.querySelector('#arttır');
+let azaltDOM = document.querySelector('#azalt');
+
+sonuçDOM.innerHTML = sonuç
+
+arttırDOM.addEventListener('click',clickEvent)
+azaltDOM.addEventListener('click',clickEvent)
 
 
+function clickEvent(){
+    if(this.id == 'arttır'){
+        sonuçDOM.innerHTML = ++sonuç
 
+    }else{
+        sonuçDOM.innerHTML = --sonuç
+    }
+}
+
+
+/* fonksiyonun kısa hali şu şekilde yazılabilir
+
+function clickEvent(){
+  this.id == 'arttır' ? sonuçDOM.innerHTML = ++sonuç : sonuçDOM.innerHTML = --sonuç
+}
+*/
 
 
