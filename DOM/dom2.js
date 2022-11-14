b40d944b88cdbc3618d091053082feb567568fca
Semanur <semanurrrarslan@gmail.com>
2022-11-14 13:53:33 +0300

    bölüm sonu çalışması localStorage

--- DOM/dom2.js
+++ DOM/dom2.js
@@ -18,7 +18,9 @@ başlıkEkle.addEventListener('click',eklenenYazı)
 
 
 // BÖLÜM SONU ÇALIŞMASI
-let sonuç = 0 ;
+let sonuç = JSON.parse(localStorage.getItem('kayıt')) ? JSON.parse(localStorage.getItem('kayıt')) : 0 ;
+//localstorage ile çağırdık
+
 let sonuçDOM = document.querySelector('#sonuç');
 let arttırDOM = document.querySelector('#arttır');
 let azaltDOM = document.querySelector('#azalt');
@@ -31,10 +33,14 @@ azaltDOM.addEventListener('click',clickEvent)
 
 function clickEvent(){
     if(this.id == 'arttır'){
-        sonuçDOM.innerHTML = ++sonuç
+        sonuçDOM.innerHTML = ++sonuç;
+        sonuçDOM.style.color = 'blue'
+        localStorage.setItem('kayıt', JSON.stringify(sonuç)) // localstorage ile kaydettik
 
     }else{
         sonuçDOM.innerHTML = --sonuç
+        sonuçDOM.style.color = 'red'
+        localStorage.setItem('kayıt', JSON.stringify(sonuç)) // localstorage ile kaydettik
     }
 }
 
@@ -46,4 +52,32 @@ function clickEvent(){
 }
 */
 
+//LOCALSTORAGE
 
+// kaynak -> https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+// web sayfasında yapılan değişikliklerin kaydının tutulmasıdır
+
+//iki kayıt türü (localstoga ve sessionstrage) arasındaki en temel fark localStorage kayıtları zaman aşımı olmaksızın tutarken sessionStorage kayıtları oturum sonlanana kadar ya da veri kaybolana kadar tutmaktadır. 
+
+/*
+veri ekleme -> window.localStorage.setItem("key","value");
+veri okuma ->  	window.localStorage.getItem("key");
+veri silme ->  localStorage.removeItem("key");
+*/
+
+
+// localstorage ile sadece string ifade tutlabilir.
+// örneğin bir diziyi kaydetmek istersek JSON.strigfy kullanmalıyız
+
+let movies = ["Kasaba", "Kış uykusu","Bir Zamanlar Anadolu"];
+
+localStorage.setItem('nuri bilge ceylan', JSON.stringify(movies)); // bu şekilde diziyi localstorage içine kaydettik
+
+// eğer bu diziyi tekrar dizi olarak çağırmak istersek JSON.parse kullanmamız gerekir
+
+JSON.parse(localStorage.getItem('nuri bilge ceylan'))
+// string olarak kaydettiğimiz diziyi bu şekilde tekrar dizi olarak çağırdık
+
+console.log(JSON.parse(localStorage.getItem('nuri bilge ceylan')))
+// çıktı => (3) ['Kasaba', 'Kış uykusu', 'Bir Zamanlar Anadolu'] yani bir dizidir
